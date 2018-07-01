364d5e1405bdd0469508c6333d1b79320e6412aa
Gaurav Padawe <39410780+GauravPadawe@users.noreply.github.com>
2018-07-01 23:03:21 +0530

    Add files via upload

---
+++ Exporting Data to Excel.py
@@ -0,0 +1,46 @@
+import pandas as pd
+file = open('D:/Gaurav - Docs/Projects/Python/Fetch Table Data/baby1998.html', 'r')
+r = file.read()
+ref = str(r)
+
+def data(ref):
+	### Rank
+	find_tr = ref.find('<tr align="right"')
+	find_td = ref.find('<td>', find_tr + 1)
+	find_td_open = ref.find('>', find_td + 1)
+	find_td_close = ref.find('</', find_td_open + 1)
+	rank = ref[find_td_open + 1:find_td_close]
+	### Male Names
+	find_td1 = ref.find('<td>', find_td_close + 1)
+	find_m_td_open = ref.find('>', find_td1 + 1)
+	find_m_td_close = ref.find('</td>', find_m_td_open + 1)
+	male = ref[find_m_td_open + 1: find_m_td_close]
+	### Female Names
+	find_td2 = ref.find('<td>', find_m_td_close + 1)
+	find_f_td_open = ref.find('>', find_td2 + 1)
+	find_f_td_close = ref.find('</td>', find_f_td_open + 1)
+	female = ref[find_f_td_open + 1: find_f_td_close]
+	return (find_f_td_close, male, female, rank)
+
+def final(ref):
+    r = []
+    m = []
+    f = []
+    while True:
+        end_pos, m1, f1, r1 = data(ref)
+        if end_pos:
+            r.append(r1)
+            m.append(m1)
+            f.append(f1)
+            ref = ref[end_pos:]
+            if r1 == '1000':
+                break
+    #male = pd.Series(m)
+    #female = pd.Series(f)
+    #rank = pd.Series(r)
+    return (pd.DataFrame({'Male':m, 'Female':f}, index=r))
+
+x = final(ref)
+#print (final(ref))
+writer = pd.ExcelWriter('D:/Gaurav - Docs/Projects/Python/Fetch Table Data/PythonExport.xlsx')
+z = x.to_excel(writer,'Sheet1')
